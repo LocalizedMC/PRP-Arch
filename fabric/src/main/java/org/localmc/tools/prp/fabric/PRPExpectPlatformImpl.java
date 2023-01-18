960248c6dc44efae0c2e8a3b20a417c57e1d5d27
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-18 11:22:57 +0800

    fix

--- fabric/src/main/java/org/localmc/tools/prp/fabric/PRPExpectPlatformImpl.java
+++ fabric/src/main/java/org/localmc/tools/prp/fabric/PRPExpectPlatformImpl.java
@@ -4,12 +4,13 @@ import org.localmc.tools.prp.PRPExpectPlatform;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PRPExpectPlatformImpl {
     /**
      * This is our actual method to {@link PRPExpectPlatform#getConfigDirectory()}.
      */
     public static Path getConfigDirectory() {
-        return FabricLoader.getInstance().getConfigDir();
+        return Paths.get(FabricLoader.getInstance().getGameDir() + "resourcepacks");
     }
 }
--- forge/src/main/java/org/localmc/tools/prp/forge/PRPExpectPlatformImpl.java
+++ forge/src/main/java/org/localmc/tools/prp/forge/PRPExpectPlatformImpl.java
@@ -4,12 +4,13 @@ import org.localmc.tools.prp.PRPExpectPlatform;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PRPExpectPlatformImpl {
     /**
      * This is our actual method to {@link PRPExpectPlatform#getConfigDirectory()}.
      */
     public static Path getConfigDirectory() {
-        return FMLPaths.CONFIGDIR.get();
+        return Paths.get(FMLPaths.GAMEDIR.get() + "resourcepacks");
     }
 }
